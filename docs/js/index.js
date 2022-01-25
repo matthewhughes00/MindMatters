d4ee608d69da650f9fb0e2d1f1372d6e48e939a1
matthewhughes00 <85319639+matthewhughes00@users.noreply.github.com>
2022-01-24 23:52:01 -0600

    coded the whole website

--- docs/js/index.js
+++ docs/js/index.js
@@ -25,3 +25,13 @@ $(document).ready(function() {
         } // End if
     });
 })
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
